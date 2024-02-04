e1950821cbf0fc1cc35e1c4bcce3b40a8af1dcb9
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-04 23:54:11 +0600

    pagination functionality started his awesome journy in the service section... you know?? that

--- client/src/page/Services.jsx
+++ client/src/page/Services.jsx
@@ -38,7 +38,7 @@ const Services = () => {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
           nobis excepturi delectus, ab id provident, voluptas iste ullam
           repellendus animi eos perspiciatis cumque. Quod sit laboriosam
-          deleniti atque .
+          deleniti atque what and what .
         </Header>
       </Container>
       <Container className="mb-64">
